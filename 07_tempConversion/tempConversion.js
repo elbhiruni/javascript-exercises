beb04dedb88b9cfbdba0cef5f45b47b6ab45beca
El Bhiruni <elbhiruni.tik@gmail.com>
2022-01-26 11:23:06 +0700

    Pass all test

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+  const c = (f - 32) * (5 / 9);
+  const result = Math.round(c * 10) / 10;
+  return result;
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+  const f = (c * (9 / 5)) + 32;
+  const result = Math.round(f * 10) / 10;
+  return result;
 };
 
 // Do not edit below this line
