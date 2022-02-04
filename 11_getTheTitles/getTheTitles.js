7c1e63d0def4d2d283d29ced58317a409c22bf27
El Bhiruni <elbhiruni.tik@gmail.com>
2022-02-04 13:36:47 +0700

    PASS

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    // const result = [];
+    // books.forEach(item => {
+    //     result.push(item.title);
+    // });
+    // return result;
+    return books.map(book => book.title);
 };
 
 // Do not edit below this line
